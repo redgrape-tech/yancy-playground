5dbf9d1a03093e89ba00c5c1f79886d1bff3aeeb
Rajesh Mallah <mallah.rajesh@gmail.com>
2020-04-15 20:30:52 +0530

    providing DBIx::Schema for sqlite db

--- yancy_play/lib/YancyPlay.pm
+++ yancy_play/lib/YancyPlay.pm
@@ -1,9 +1,13 @@
 package YancyPlay;
 use Mojo::Base 'Mojolicious';
+use DBIx::Class::Schema::Loader qw/ make_schema_at /;
+
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
+
+	my $log = $self->log;
 
   # Load configuration from hash returned by config file
   my $config = $self->plugin('Config');
@@ -16,6 +20,25 @@ sub startup {
 
   # Normal route to controller
   $r->get('/')->to('example#welcome');
+
+	my $dsn = 'dbi:SQLite:dbname=sakila.db';
+
+	make_schema_at( 'Play::Schema', { debug => 0, naming=>'current' }, [ $dsn , '', '' ] );
+
+	my $schema ;
+
+	$self->helper( schema => sub {  $schema //= Play::Schema->connect( $dsn);   } );
+
+	my $rs = $self->schema->resultset('Film')->search( {},
+					{
+									columns => [ qw/title/ ],
+									distinct => 1
+					}
+	);
+	my $count = $rs->count;
+	$log->debug("sample query result: distinct film title is: $count");
+
+
 }
 
 1;
